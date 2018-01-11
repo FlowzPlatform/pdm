234cd7e4152b6b27d9b117b86e2753bc9a19d784
chiragob <chirag@officebrain.com>
2018-01-11 12:08:32 +0530

    Revert "Done user identification"

--- src/services/pdm/pdm.class.js
+++ src/services/pdm/pdm.class.js
@@ -1,13 +1,10 @@
 // import { create } from 'domain';
 /* eslint-disable no-unused-vars */
-const config = require('../../../config/default.json');
-const errors = require('@feathersjs/errors');
 const feathers = require('feathers');
 const rest = require('feathers-rest');
-
 var jwt1 = require('jsonwebtoken');
 var request = require('request');
-
+const config = require('../../../config/default.json');
 if (process.env.esUrl != '')
     config.esUrl = process.env.esUrl
 if(process.env.secret != '')
@@ -16,38 +13,22 @@ if(process.env.auth_url != '')
     config.auth_url = process.env.auth_url
 if(process.env.pwd != '')
     config.pwd = process.env.pwd
-
-var credOptions = {
+const credOptions = {
   'index' : process.env.index,
-  'username': '',
+  'usernname': process.env.user,
   'password': process.env.pwd
 }
 
 var esURL = config.esUrl
 esURL = esURL.substr(8)
-let uri = 'https://' + credOptions.username + ':' + credOptions.password + '@' + esURL + '/' + credOptions.index + '/_search'
+const uri = 'https://' + credOptions.usernname + ':' + credOptions.password + '@' + esURL + '/' + credOptions.index + '/_search'
 
 class Service {
   constructor (options) {
     this.options = options || {};
   }
 
-  setup(app){
-    this.app = app;
-  }
-
   async find (params) {
-    if (params.headers.vid) {
-      await this.app.service('vshopdata').get(params.headers.vid)
-      .then(response => {
-        credOptions.username = response.esUser
-      }).catch(err => {
-          console.log(err)
-      })
-    } else {
-      throw new errors.Forbidden('Unauthorized access')
-    }
-
     let getAllResult
     let queryBody = {
       "query" : {
@@ -67,7 +48,7 @@ class Service {
       }
     }
     if (Object.keys(params.query).length == 0) {
-      throw new errors.NotFound('No parameters to search')  
+      getAllResult = ['Please give parameters to search']     
     } else {
       Object.keys(params.query).forEach(function(key){
         queryBody.query.bool.filter.bool.should[0].bool.must.push({ "match" : {[key] : params.query[key]} })
@@ -78,17 +59,6 @@ class Service {
   }
 
   async get (language, params) {
-    if (params.headers.vid) {
-      await this.app.service('vshopdata').get(params.headers.vid)
-      .then(response => {
-        credOptions.username = response.esUser
-      }).catch(err => {
-        console.log(err)
-      })
-    } else {
-      throw new errors.Forbidden('Unauthorized access')
-    }
-
     let queryBody = {
       "query" : {
         "bool" : {
@@ -107,9 +77,6 @@ class Service {
         }
       }
     }
-    if (language == undefined) {
-      delete queryBody.query.bool.filter.bool.should[0].bool.must[0]
-    }
     if (!Object.keys(params.query).length == 0) {
       Object.keys(params.query).forEach(function(key){
         queryBody.query.bool.filter.bool.should[0].bool.must.push({ "match" : {[key] : params.query[key]} })
@@ -120,17 +87,6 @@ class Service {
   }
 
   async create (data, params) {
-    if (params.headers.vid) {
-      await this.app.service('vshopdata').get(params.headers.vid)
-      .then(response => {
-        credOptions.username = response.esUser
-      }).catch(err => {
-          console.log(err)
-      })
-    } else {
-      throw new errors.Forbidden('Unauthorized access')
-    }
-
     let query = ''
     let country = ''
     if (data.query !== undefined && data.query !== '') {
@@ -151,11 +107,11 @@ class Service {
       res.send(searchResult)
     })
     app.post('/:index//:action', async function (req, res, err) {
-      var country
+      var country = 'CA'
       if (err & err === 'router') {
         return done(err)
       }
-      let searchResult = await self.get(country, {headers: {vid: req.feathers.headers.vid}, query: req.query})
+      let searchResult = await self.get(country, req.feathers)
       res.send(searchResult)
     })
     app.post('/:index/', async function (req, res, err) {
@@ -168,20 +124,19 @@ class Service {
     })
   }
 
-  // getAllResultFromES(params) {
-  //   return new Promise((resolve, reject) => {
-  //     request({method: 'post', url: uri}, function (error, response, body) {
-  //       if (error) {
-  //         resolve(error)
-  //       } else {
-  //         resolve(response.body)
-  //       }
-  //     })
-  //   })
-  // }
+  getAllResultFromES(params) {
+    return new Promise((resolve, reject) => {
+      request({method: 'post', url: uri}, function (error, response, body) {
+        if (error) {
+          resolve(error)
+        } else {
+          resolve(response.body)
+        }
+      })
+    })
+  }
   
   getResultFromES(query, params) {
-    uri = 'https://' + credOptions.username + ':' + credOptions.password + '@' + esURL + '/' + credOptions.index + '/_search'
     return new Promise((resolve, reject) => {
       request({method: 'post', url: uri, json: true, body: query}, function (error, response, body) {
         if (error) {
@@ -194,17 +149,6 @@ class Service {
   }
 
   async getDataFromES (query, params, country) {
-    if (params.headers.vid) {
-      await this.app.service('vshopdata').get(params.headers.vid)
-      .then(response => {
-        credOptions.username = response.esUser
-      }).catch(err => {
-          console.log(err)
-      })
-    } else {
-      throw new errors.Forbidden('Unauthorized access')
-    }
-
     let queryBody = {
       "query" : {
         "bool" : {
