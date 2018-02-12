a8f378b3cfda31273bdb78e6b9fa186725804984
Sanjaysinh Zala <sanjaysinhz@officebrain.com>
2018-02-12 17:39:59 +0530

    Done jQ entry after new supplier creates

--- src/services/vshopdata/vshopdata.hooks.js
+++ src/services/vshopdata/vshopdata.hooks.js
@@ -1,6 +1,7 @@
 const auth = require('@feathersjs/authentication');
 const jwt = require('@feathersjs/authentication-jwt');
 var rp = require('request-promise');
+var axios = require('axios');
 let errors = require('@feathersjs/errors') ;
 const config = require('../../config.js');
 
@@ -56,7 +57,7 @@ async function create(hook){
     throw new errors.NotAuthenticated('Invalid token');
   }else{
     let a = hook.data.virtualShopName
-    let b = hook.data.userId;
+    let b = userData.id;
     c = hook.data.suppliers;
     hook.data = {};
     hook.data.virtualShopName= a;
@@ -66,58 +67,63 @@ async function create(hook){
 }
 
 async function after(hook){
-  let res = await validateUser(hook);
-  if(res.code == 401){
-    throw new errors.NotAuthenticated('Invalid token');
-  }else{
-    hook.app.service('vshop-detail').create({
-      "id":hook.result.id,
-      "suppliers":c
-    })
+  if(hook.data.id) {
+    let res = await validateUser(hook);
+    if(res.code == 401){
+      throw new errors.NotAuthenticated('Invalid token');
+    }else{
+      hook.app.service('vshop-detail').create({
+        "id":hook.result.id,
+        "suppliers":c
+      })
 
-    let body = {
-      "connection" : {
-        "host": config.jobQueue.host,
-        "port": config.jobQueue.port,
-        "db": config.jobQueue.db
-      },
-      "queue" : {
-        "name": "importData"
-      },
-      "vId": hook.result.id,
-      "userdetail":{
-        "emailId": userData.email,
-        "username": userData.username
+      let body = {
+        "connection" : {
+          "host": config.rethinkdb.servers[0].host,
+          "port": config.rethinkdb.servers[0].port,
+          "db": config.rethinkdb.db
+        },
+        "queue" : {
+          "name": "importData"
+        },
+        "vId": hook.result.id,
+        "userdetail":{
+          "emailId": userData.email,
+          "password": userData.password,
+          "userId": userData.id
+        }
       }
-    }
-
-    let response = {
-      method: 'POST',
-      url: config.jobQueue.url,
-      json: true,
-      body: body
-    }
-    //----------------------  send in job queue
-    const jobQueueRes = await rp(response);
+      let response = {
+        method: 'POST',
+        url: config.jobQueue.url,
+        json: true,
+        body: body
+      }
+      const jobQueueRes = rp(response);
+      //----------------------  send in job queue  const jobQueueRes = rp(response);
+      // axios.post(config.jobQueue.url, body)
+      // .then(res => {
+      //   console.log('Job-queue entry done')
+      // })
+      // .catch(err => {
+      //   throw new errors.NotAcceptable('Error during insertion in job-queue')
+      // })
+    }     
   }
 }
 
-validateUser = data =>{
-  var options = {
-    uri: config.userDetailApi,
-    headers: {
-      Authorization : apiHeaders.authorization
+validateUser = async data =>{
+  return await axios.get(config.userDetailApi, {headers: {Authorization: apiHeaders.authorization}})
+  .then(parsedBody => {
+    userData = {
+      'id': parsedBody.data.data._id,
+      'email': parsedBody.data.data.email,
+      'password': parsedBody.data.data.password
     }
-  };
-  return new Promise((resolve , reject) =>{
-    rp(options)
-    .then(function (parsedBody) {
-      userData = parsedBody[0]
-      resolve(parsedBody)
-    })
-    .catch(function (err) {
-      resolve({"code" : 401 })
-    });
+    return userData
+  })
+  .catch(function (err) {
+    return {"code" : 401 }
   })
 }
 
