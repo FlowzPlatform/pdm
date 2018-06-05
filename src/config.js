82f3bec9c2e1e479d77856cc0b3ad17def48276f
Sanjaysinh Zala <sanjaysinhz@officebrain.com>
2018-06-05 17:59:36 +0530

    update code with ESLint syntax

--- src/config.js
+++ src/config.js
@@ -1,39 +1,39 @@
 
 module.exports = {
-    "host": "localhost",
-    "port": 3038,
-    "public": "../public/",
-    "domainKey": process.env.domainKey,
-    "paginate": {
-        "default": 10,
-        "max": 50
-    },
-    "rethinkdb": {
-        "db": "feather_product",
-        "servers": [
-            {
-                "host": process.env.RDB_HOST,
-                "port": process.env.RDB_PORT
-            }
-        ]
-    },
-    "jobQueue": {
-        "url": "http://api." + process.env.domainKey + "/rjobqueue1/job/create",
-        "db": process.env.JQ_DB,
-        "host": process.env.JQ_HOST,
-        "port": process.env.JQ_PORT
-    },
-    "credOptions": {
-        "index" : process.env.index,
-        "username": "",
-        "password": "",
-        "type": ""
-    },
-    "esUrl": process.env.esUrl,
-    "secret": process.env.secret,
-    "auth_url": "http://api." + process.env.domainKey + "/user/getuserdetails/ index=pdm1",
-    "userDetailApi": "http://api." + process.env.domainKey + "/auth/api/userdetails",
-    "loginUrl": "https://api." + process.env.domainKey + "/auth/api/login",
-    "pwd": process.env.pwd,
-    "wsPort": process.env.wsport || "4038"
-}
+  'host': 'localhost',
+  'port': 3038,
+  'public': '../public/',
+  'domainKey': process.env.domainKey,
+  'paginate': {
+    'default': 10,
+    'max': 50
+  },
+  'rethinkdb': {
+    'db': 'feather_product',
+    'servers': [
+      {
+        'host': process.env.RDB_HOST,
+        'port': process.env.RDB_PORT
+      }
+    ]
+  },
+  'jobQueue': {
+    'url': 'http://api.' + process.env.domainKey + '/rjobqueue1/job/create',
+    'db': process.env.JQ_DB,
+    'host': process.env.JQ_HOST,
+    'port': process.env.JQ_PORT
+  },
+  'credOptions': {
+    'index' : process.env.index,
+    'username': '',
+    'password': '',
+    'type': ''
+  },
+  'esUrl': process.env.esUrl,
+  'secret': process.env.secret,
+  'auth_url': 'http://api.' + process.env.domainKey + '/user/getuserdetails/ index=pdm1',
+  'userDetailApi': 'http://api.' + process.env.domainKey + '/auth/api/userdetails',
+  'loginUrl': 'https://api.' + process.env.domainKey + '/auth/api/login',
+  'pwd': process.env.pwd,
+  'wsPort': process.env.wsport || '4038'
+};
--- src/services/product/product.class.js
+++ src/services/product/product.class.js
@@ -1,13 +1,7 @@
-/* eslint-disable no-unused-vars */
-const feathers = require('@feathersjs/feathers');
-const express = require('@feathersjs/express');
 const errors = require('@feathersjs/errors');
-const rest = require('@feathersjs/express/rest');
 const config = require('../../config.js');
 const vm = require('../vidMiddleware.js');
 var jwt = require('jsonwebtoken');
-var request = require('request')
-var username = ''
 
 class Service {
   constructor (options) {
@@ -15,216 +9,216 @@ class Service {
   }
   
   async find (params) {
-    let region = params.query.region
+    let region = params.query.region;
     let bodyData = {
-      "query" : {
-         "bool" : {
-            "filter" : {
-               "bool" : {
-                 "should" : [
-                   { "bool" : {
-                     "must" : [
-                        { "match" : {"available_regions" : region}},
-                       { "bool" : {
-                     "must_not" : [
-                       { "match" : {"non-available_regions" : region}}
-                     ]
-                   }}
-                     ]
-                   }},
-                   { "bool" : {
-                     "must" : [
-                        { "match" : {"available_regions" : ""}},
-                       { "match" : {"non-available_regions" : ""}}
-                     ]
-                   }}
-                 ]
-              }
-            },
-              "must": {
-                     "match_all": {
-
-                     }
-                   }
-
-         }
-      }
-   }
-    let searchResult = this.getResultFromES(bodyData,params)
-    return Promise.resolve(searchResult);
-  }
-
-
-async getByIdAndCountry(region,id,req) {
-  let bodyData ={
-    "query" : {
-      "bool" : {
-        "filter" : {
-            "bool" : {
-              "should" : [
-                { "bool" : {
-                  "must" : [
-                    { "match" : {"available_regions" :region }},
-                    { "bool" : {
-                      "must_not" : [
-                        { "match" : {"non-available_regions" : region}}
+      'query' : {
+        'bool' : {
+          'filter' : {
+            'bool' : {
+              'should' : [
+                { 'bool' : {
+                  'must' : [
+                    { 'match' : {'available_regions' : region}},
+                    { 'bool' : {
+                      'must_not' : [
+                        { 'match' : {'non-available_regions' : region}}
                       ]
                     }}
                   ]
                 }},
-                { "bool" : {
-                  "must" : [
-                    { "match" : {"available_regions" : ""}},
-                    { "match" : {"non-available_regions" : ""}}
+                { 'bool' : {
+                  'must' : [
+                    { 'match' : {'available_regions' : ''}},
+                    { 'match' : {'non-available_regions' : ''}}
                   ]
                 }}
               ]
+            }
+          },
+          'must': {
+            'match_all': {
+
+            }
           }
-        },
-        "must": {
-          "match": {
-              "_id": id
+
+        }
+      }
+    };
+    let searchResult = this.getResultFromES(bodyData);
+    return Promise.resolve(searchResult);
+  }
+
+
+  async getByIdAndCountry(region,id) {
+    let bodyData ={
+      'query' : {
+        'bool' : {
+          'filter' : {
+            'bool' : {
+              'should' : [
+                { 'bool' : {
+                  'must' : [
+                    { 'match' : {'available_regions' :region }},
+                    { 'bool' : {
+                      'must_not' : [
+                        { 'match' : {'non-available_regions' : region}}
+                      ]
+                    }}
+                  ]
+                }},
+                { 'bool' : {
+                  'must' : [
+                    { 'match' : {'available_regions' : ''}},
+                    { 'match' : {'non-available_regions' : ''}}
+                  ]
+                }}
+              ]
+            }
+          },
+          'must': {
+            'match': {
+              '_id': id
+            }
           }
         }
       }
-    }
+    };
+    // let username =  await this.check(req,bodyData)
+    let searchResult = await this.getResultByBody(bodyData,config.credOptions.username);
+    return Promise.resolve(searchResult);
   }
-  // let username =  await this.check(req,bodyData)
-  let searchResult = await this.getResultByBody(bodyData,config.credOptions.username)
-  return Promise.resolve(searchResult)
-}
 
-_setup(app, path) {
-  var self = this;
-  app.get('/' + path + '/:region/:id', async function (req, res, err) {
-    let flag = false
-    let region = req.params.region
-    let id = req.params.id
-    if (err && err === 'route') {
-      return done();
-    }
-    jwt.verify(req.feathers.headers.authorization, config.secret, function(err, decoded) {
-      if(err) {
-        flag = true
+  _setup(app, path) {
+    var self = this;
+    app.get('/' + path + '/:region/:id', async function (req, res, err) {
+      let flag = false;
+      let region = req.params.region;
+      let id = req.params.id;
+      if (err && err === 'route') {
+        return done(); // eslint-disable-line no-undef
       }
-    })
-    await vm.check(app.service('vshopdata'), req.feathers.headers.vid, false)
-    .then(response => {
-      config.credOptions.username = response[0]
-      config.credOptions.password = response[1]
-      req.params.credential = response
-    })
-    if (flag) {
-      var er = new errors.NotAuthenticated('No auth token')
-      res.send(er)
-    } else if (req.params.credential[2]) {
-      res.send(req.params.credential[2])
-    } else {
-      let response = await self.getByIdAndCountry(region,id,req)
-      console.log('info: after: api/products - Method: custom get')
-      res.send(response)
-    }
-  });
-}
+      jwt.verify(req.feathers.headers.authorization, config.secret, function(err, decoded) { // eslint-disable-line no-unused-vars
+        if(err) {
+          flag = true;
+        }
+      });
+      await vm.check(app.service('vshopdata'), req.feathers.headers.vid, false)
+        .then(response => {
+          config.credOptions.username = response[0];
+          config.credOptions.password = response[1];
+          req.params.credential = response;
+        });
+      if (flag) {
+        var er = new errors.NotAuthenticated('No auth token');
+        res.send(er);
+      } else if (req.params.credential[2]) {
+        res.send(req.params.credential[2]);
+      } else {
+        let response = await self.getByIdAndCountry(region,id);
+        console.log('info: after: api/products - Method: custom get'); // eslint-disable-line no-console
+        res.send(response);
+      }
+    });
+  }
 
-get (region,params) {
-  let searchResult = this.getDataFromES(region, params)
-  return Promise.resolve(searchResult)
-}
+  get (region,params) {
+    let searchResult = this.getDataFromES(region, params);
+    return Promise.resolve(searchResult);
+  }
 
-   async getResultFromES (bodyData,params) {
+  async getResultFromES (bodyData) {
 
     // setEsClient(body)
     let  elasticsearch = this.options.elasticsearch;
     let  host = this.options.esUrl;
-    let userName = config.credOptions.username
-    let passWord = config.credOptions.password
+    let userName = config.credOptions.username;
+    let passWord = config.credOptions.password;
     const esClient = new elasticsearch.Client({
       host: host,
-      httpAuth:userName+":"+passWord
-    })
+      httpAuth:userName+':'+passWord
+    });
 
-    let esOptions = this.options.esOption
+    let esOptions = this.options.esOption;
 
     let response = await esClient.search({
       index:  esOptions.index,
       type:  esOptions.type,
       body: bodyData
-    })
-    return response
+    });
+    return response;
   }
 
   async getResultByBody (bodyData,username) {
     let elasticsearch = this.options.elasticsearch;
     let host = this.options.esUrl;
-    let userName = username
-    let passWord = config.credOptions.password
+    let userName = username;
+    let passWord = config.credOptions.password;
     const esClient = new elasticsearch.Client({
       host: host,
-      httpAuth:userName+":"+passWord
-    })
+      httpAuth:userName+':'+passWord
+    });
 
-    let esOptions = this.options.esOption
+    let esOptions = this.options.esOption;
     let response = await esClient.search({
       index: esOptions.index,
       type: esOptions.type,
       body: bodyData
-    })
-    return response
+    });
+    return response;
   }
 
   async getDataFromES (region, params, query) {
     let bodyData = {
-      "query" : {
-        "bool" : {
-           "filter" : {
-              "bool" : {
-                "should" : [
-                  { "bool" : {
-                    "must" : [
-                       { "match" : {"available_regions" : region}},
-                      { "bool" : {
-                    "must_not" : [
-                      { "match" : {"non-available_regions" : region}}
-                    ]
-                  }}
-                    ]
-                  }},
-                  { "bool" : {
-                    "must" : [
-                       { "match" : {"available_regions" : ""}},
-                      { "match" : {"non-available_regions" : ""}}
-                    ]
-                  }}
-                ]
-             }
-           },
-            "must": {
-                    "match_all": {
+      'query' : {
+        'bool' : {
+          'filter' : {
+            'bool' : {
+              'should' : [
+                { 'bool' : {
+                  'must' : [
+                    { 'match' : {'available_regions' : region}},
+                    { 'bool' : {
+                      'must_not' : [
+                        { 'match' : {'non-available_regions' : region}}
+                      ]
+                    }}
+                  ]
+                }},
+                { 'bool' : {
+                  'must' : [
+                    { 'match' : {'available_regions' : ''}},
+                    { 'match' : {'non-available_regions' : ''}}
+                  ]
+                }}
+              ]
+            }
+          },
+          'must': {
+            'match_all': {
 
-                    }
-                }
+            }
+          }
         }
       }
-    }
+    };
 
     if (query !== undefined && query !== '') {
-      bodyData.query.bool.must = query
+      bodyData.query.bool.must = query;
     }
 
-      let searchResult = this.getResultFromES(bodyData, params)
-      return Promise.resolve(searchResult)
+    let searchResult = this.getResultFromES(bodyData);
+    return Promise.resolve(searchResult);
   }
 
   create (data, params) {
     if (data.country === undefined || data.country === '') {
-      return {'message': 'country required'}
+      return {'message': 'country required'};
     }
-    let query = ''
+    let query = '';
     if (data.query !== undefined && data.query !== '') {
-      query = data.query
+      query = data.query;
     }
-    data = this.getDataFromES (data.country, params, query)
+    data = this.getDataFromES (data.country, params, query);
     if (Array.isArray(data)) {
       return Promise.all(data.map(current => this.create(current)));
     }
